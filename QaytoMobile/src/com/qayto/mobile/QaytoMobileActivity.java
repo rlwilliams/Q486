09c7896ad3b11ead98b936e9a42992429503f885
rlwilliams <Robert@PaleBlue.(none)>
2012-04-09 20:53:20 -0700

    Removed images and a couple other fixes

--- QaytoMobile/src/com/qayto/mobile/QaytoMobileActivity.java
+++ QaytoMobile/src/com/qayto/mobile/QaytoMobileActivity.java
@@ -1,7 +1,5 @@
 package com.qayto.mobile;
 
-import org.json.JSONArray;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
--- QaytoMobile/src/com/qayto/mobile/WaitingActivity.java
+++ QaytoMobile/src/com/qayto/mobile/WaitingActivity.java
@@ -6,9 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.AdapterView.OnItemClickListener;
 
 
 public class WaitingActivity extends Activity {
